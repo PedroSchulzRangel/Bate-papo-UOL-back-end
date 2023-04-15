9bd54bec4a544758d02316b8bf3a04fcc4335424
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-04-15 16:17:52 -0300

    Requisito: POST / participants

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+import joi from "joi";
+import dayjs from "dayjs";
 
 const app = express();
 
@@ -18,6 +20,41 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 mongoClient.connect()
 .then(() => db = mongoClient.db())
 .catch((error) => console.log(error.message))
+
+app.post("/participants", async (req, res) => {
+
+    const { name } = req.body;
+
+    const participantSchema = joi.object({
+        name: joi.string().required()
+    })
+    const validation = participantSchema.validate(req.body);
+
+    if(validation.error){
+        return res.sendStatus(422);
+    }
+
+    try{
+        const nameAlreadyInUse = await db.collection("participants").findOne({name});
+
+        if(nameAlreadyInUse){
+            return res.sendStatus(409);
+        }
+        
+        await db.collection("participants").insertOne({name, lastStatus: Date.now()});
+
+        await db.collection("messages").insertOne({
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: dayjs().format("HH:mm:ss")});
+
+        res.sendStatus(201);
+    } catch(error){
+        res.status(500).send(error.message);
+    }
+});
 
 app.get("/participants", async (req, res) => {
     try{
