3e782f6f1978a1688ee1849438fe2d133e4054de
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-04-16 18:04:34 -0300

    Requisito: POST/messages

--- src/app.js
+++ src/app.js
@@ -27,7 +27,8 @@ app.post("/participants", async (req, res) => {
 
     const participantSchema = joi.object({
         name: joi.string().required()
-    })
+    });
+
     const validation = participantSchema.validate(req.body);
 
     if(validation.error){
@@ -65,6 +66,36 @@ app.get("/participants", async (req, res) => {
     }
 });
 
+app.post("/messages", async (req, res) => {
+
+const {to, text, type} = req.body;
+
+const { user } = req.headers;
+
+const messageSchema = joi.object({
+to: joi.string().required(),
+text: joi.string().required(),
+type: joi.string().required().pattern(/^message$/).pattern(/^private_message$/),
+from: joi.string().required()
+})
+
+const validation = messageSchema.validate({...req.body, from: user});
+
+try{
+    const participantExists = await db.collection("participants").findOne({name: user});
+
+    if(validation.error || !participantExists){
+        return res.sendStatus(422);
+    }
+
+    await db.collection("messages").insertOne({from: user, to, text, type, time: dayjs().format("HH:mm:ss")});
+
+    res.sendStatus(201);
+    
+} catch(error){
+    res.status(500).send(error.message);
+}
+});
 const PORT = 5000;
 
 app.listen(PORT,() => console.log(`Servidor rodando na porta ${PORT}`));
